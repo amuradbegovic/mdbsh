ef840a45de602437674795facb0b4824542b38a7
Adnan Muradbegović <adnanm92005@gmail.com>
2025-04-13 20:19:01 +0200

    Implemented getcwd

--- main.cpp
+++ main.cpp
@@ -1,57 +1,70 @@
+#include <csignal>
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <string_view>
-#include <sstream>
 #include <vector>
-#include <iomanip>
-#include <csignal>
 
-#include <unistd.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #include "posix_wrapper.h"
 
-// TODO: implement proper tokenizer. This is crucial to support pipes and redirection later on
+// TODO: implement proper tokenizer. This is crucial to support pipes and
+// redirection later on
 std::vector<std::string> Tokenize(const std::string &line) {
-    std::vector<std::string> tokens;
-    std::istringstream line_stream(line);
-    std::string token;
-    while (line_stream >> token)
-        tokens.push_back(token);
+  std::vector<std::string> tokens;
+  std::istringstream line_stream(line);
+  std::string token;
+  while (line_stream >> token)
+    tokens.push_back(token);
 
-    return tokens;
+  return tokens;
 }
 
 int main() {
-    std::string prompt = "$ ";
-    std::string line;
+  std::string prompt = "$ ";
+  std::string line;
 
-    while (std::cout << prompt && getline(std::cin, line)) {
-        auto tokens = Tokenize(line);
-
-        // No tokens, no action
-        if (tokens.size() == 0) continue;
-
-        // exit has to be implemented as a built-in command
-        // This is a dead simple implementation. If the first token received from the user is literally "exit",
-        // the shell will break out of the loop and terminate as if it received EOF.
-        if (tokens.at(0) == "exit")
-            break;
-        
-        // Create a child process, and call execvp inside it to execute the (non-built-in) 
-        // command/file previously received from the user 
-        if (fork() == 0) 
-            // Try to execute the command and print error message and exit the child process on failure
-            if (posix_wrapper::execvp(tokens.at(0), tokens) == -1) {
-                if (tokens.at(0).find('/') == std::string::npos)
-                    std::cerr << tokens.at(0) << ": command not found\n";
-                else std::cerr << tokens.at(0) <<  ": no such file or directory\n";
-                exit(EXIT_FAILURE);
-            }
-
-        // The shell must wait for the child process to exit, otherwise some unwanted behavior happens
-        wait(NULL);
+  while (getline(std::cin, line)) {
+    try {
+      prompt = posix_wrapper::getcwd() + " $ ";
+    } catch (std::runtime_error re) {
+      std::cerr << "error: couldn't get current working directory\n";
     }
+    std::cout << prompt;
 
-    return 0;
+    auto tokens = Tokenize(line);
+
+    // No tokens, no action
+    if (tokens.size() == 0)
+      continue;
+
+    // exit has to be implemented as a built-in command
+    // This is a dead simple implementation. If the first token received from
+    // the user is literally "exit", the shell will break out of the loop and
+    // terminate as if it received EOF.
+    if (tokens.at(0) == "exit")
+      break;
+
+    // Create a child process, and call execvp inside it to execute the
+    // (non-built-in) command/file previously received from the user
+    if (fork() == 0)
+      // Try to execute the command and print error message and exit the child
+      // process on failure
+      if (posix_wrapper::execvp(tokens.at(0), tokens) == -1) {
+        if (tokens.at(0).find('/') == std::string::npos)
+          std::cerr << tokens.at(0) << ": command not found\n";
+        else
+          std::cerr << tokens.at(0) << ": no such file or directory\n";
+        exit(EXIT_FAILURE);
+      }
+
+    // The shell must wait for the child process to exit, otherwise some
+    // unwanted behavior happens
+    wait(NULL);
+  }
+
+  return 0;
 }
--- posix_wrapper.h
+++ posix_wrapper.h
@@ -1,13 +1,18 @@
-/* 
- * These are C++ wrapper functions for some POSIX API functions and syscallshthat deal with strings
- * What they essentially do is convert C++ strings and vectors to classic C null-terminated strings and "vectors" organized as pointers to pointers or similar so that they can be passed to POSIX API functions.
- * I am not going to write wrappers for all POSIX functions. I am only going to implement wrappers for functions that deal with data types which are nicer to work with in modern C++ than ANSI C. 
-*/
+/*
+ * These are C++ wrapper functions for some POSIX API functions and
+ * syscallshthat deal with strings What they essentially do is convert C++
+ * strings and vectors to classic C null-terminated strings and "vectors"
+ * organized as pointers to pointers or similar so that they can be passed to
+ * POSIX API functions. I am not going to write wrappers for all POSIX
+ * functions. I am only going to implement wrappers for functions that deal with
+ * data types which are nicer to work with in modern C++ than ANSI C.
+ */
 
-#include <vector>
 #include <string>
+#include <vector>
 
 namespace posix_wrapper {
-    int execvp(const std::string &file, const std::vector<std::string> &argv);
-    std::string getcwd(); 
-}
+int execvp(const std::string &file, const std::vector<std::string> &argv);
+std::string getcwd();
+std::string getenv(const std::string &name);
+} // namespace posix_wrapper
